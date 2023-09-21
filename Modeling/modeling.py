0bf274fe518ecddbfd8fa7db40ff4d8a38b6d138
CHA <chchloe2020@gmail.com>
2023-09-21 15:41:35 +0900

    Update the code and add functions to get url

--- Modeling/modeling.py
+++ Modeling/modeling.py
@@ -39,18 +39,13 @@ def load_doc2vec(file_name='naver.doc2vec'):
     return model
 
 
-"""
-    기사 추천 받기
+# 기사 url 로 유사한 기사 10개 추천받기
+def show_similar_results(data, model, url, column_list=['title', 'date_upload', 'content', 'cos_simil']):
     """
-# column_list = ['title', 'date_upload', 'content', 'cos_simil'] # 추천받는 기사의 컬럼 선택
-
-# url로 기사 인덱스 정보 찾기
-def get_news_idx_by_url(data, url):
+    기사 url을 이용해서
+    유사한 기사 10개 추천받기
+    """
     news_idx = data[data['url'] == url].index.values.astype(int)[0]
-    return news_idx
-
-# 기사 인덱스로 유사한 기사 10개 추천받기
-def show_similar_results(data, model, news_idx, column_list=['title', 'date_upload', 'content', 'cos_simil']):
     similar_docs = model.dv.most_similar(news_idx)
     result = pd.DataFrame()
     for idx, val in similar_docs:
